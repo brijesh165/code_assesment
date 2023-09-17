7c13ec72cb592852ecf670f0bcae91cd72af8a33
brijesh-FanCraze <brijesh@fancraze.com>
2023-09-17 19:30:10 +0530

    PostCss changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,8 @@
 module.exports = {
-  // plugins: {
-  //   tailwindcss: {},
-  //   autoprefixer: {},
-  // },
-  plugins: [
-    "postcss-import",
-    "tailwindcss/nesting",
-    "tailwindcss",
-    "autoprefixer",
-  ],
+  plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
